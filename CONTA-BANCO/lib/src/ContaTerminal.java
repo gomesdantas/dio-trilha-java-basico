05bddc9ef001c4cadc9573d4270614a99edba4ed
gomesdantas <biancagomesdantas@gmail.com>
2024-05-09 13:00:13 -0300

    projeto

--- CONTA-BANCO/lib/src/ContaTerminal.java
+++ CONTA-BANCO/lib/src/ContaTerminal.java
@@ -1,5 +1,28 @@
-public class App {
+import java.util.Scanner; 
+
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o saldo da conta: ");
+        double saldo = scanner.nextDouble();
+
+      
+        System.out.print("Digite o valor solicitado para saque: ");
+        double valorSolicitado = scanner.nextDouble();
+    
+            if (saldo >= valorSolicitado) {
+           
+                saldo = saldo - valorSolicitado;
+                System.out.println("Saque realizado com sucesso. Novo saldo: " + saldo);
+            } else {
+              
+                System.out.println("Saldo insuficiente. Não é possível realizar o saque.");
+            }
+            System.out.println("Valor do saldo é " + saldo);
+            
+            scanner.close();
     }
+    
 }
